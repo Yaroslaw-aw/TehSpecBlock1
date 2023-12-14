1fea0043b60ce2ab9f2f83db1877c32dfe4a876a
Yaroslaw-aw <manumission@mail.ru>
2023-12-15 00:36:21 +0300

    lol fix

---
+++ Seminar 1 DZ/Seminar 1 DZ/Operations/Substract.cs
@@ -0,0 +1,10 @@
+﻿namespace Seminar_1_DZ.Operations
+{
+    internal class Sub
+    {
+        public static void Substract2Numbers(double num1,  double num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+    }
+}
