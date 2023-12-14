24d9dd3c46fc2b24e41a5c0f35992ed7f8c7b25c
Yaroslaw-aw <manumission@mail.ru>
2023-12-14 22:40:17 +0300

    First commit

---
+++ Seminar 1 DZ/Seminar 1 DZ/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Seminar_1_DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                throw new Exception("Ничего не введено");
+            }
+
+            if (!double.TryParse(args[0], out double number1) || !double.TryParse(args[2], out double number2))
+            {
+                throw new Exception("Неверный ввод");
+            }
+
+            switch (args[1])
+            {
+                case "+":
+                    {
+                        Console.WriteLine(number1 + number2);
+                        break;
+                    }
+                case "-":
+                    {
+                        Console.WriteLine(number1 - number2);
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine(number1 * number2);
+                        break;
+                    }
+                case "/":
+                    {
+                        if (number2 != 0)
+                        {
+                            Console.WriteLine(number1 / number2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Попытка деления на ноль. А так нельзя");
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
